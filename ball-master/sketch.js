59fe858d65a51bd0a0aa2e9a34fecd53a6c6715c
LissetBe <32806754+LissetBe@users.noreply.github.com>
2017-10-14 19:59:06 -0700

    Initial commit

---
+++ ball-master/sketch.js
@@ -0,0 +1,21 @@
+var balls = [];
+
+function setup() {
+ var cnv=createCanvas(900, 800);
+ var x=(windowWidth-width)/2
+ var y=30
+ cnv.position(x,y);
+ for(var i = 0; i<50; i++){
+   balls.push (new Ball());
+ }
+}
+
+function draw(){
+  fill(20, 50, 220, 10);
+  rect(0, 0, width, height);
+  for(var i = 0; i<balls.length; i++){
+  balls[i].update();
+  }
+
+
+}
